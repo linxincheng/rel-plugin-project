909a3cfa2d138dc20effbed8e3c0f3718bb25e52
Xiaoxin <qwewsx789852@qq.com>
2022-08-21 14:47:33 +0800

    feat: 增加鼠标移入获取内容

--- public/insert.js
+++ public/insert.js
@@ -1,1 +1,36 @@
-console.log('insert.js loaded')
+console.log('insert.js loaded');
+let x = 0;
+let y = 0;
+
+const DOM = document.createElement('div');
+DOM.classList.add('popup');
+const collectitonDOM = document.createElement('a');
+collectitonDOM.classList.add('collection');
+collectitonDOM.innerText='收藏';
+DOM.appendChild(collectitonDOM);
+
+document.body.appendChild(DOM);
+document.body.onclick = function ($event) {
+    x = $event.pageX;
+    y = $event.pageY;
+    console.log(x,y);
+    clearTimeout(timer);
+    DOM.setAttribute('style', `top: ${y}px; left: ${x}px;`)
+    DOM.setAttribute('title',$event?.target?.innerText || '')
+    DOM.classList.add('show');
+};
+DOM.addEventListener('mouseleave', () => {
+    console.log('dom mouseleave');
+    hidden();
+})
+DOM.addEventListener('mouseover', () => {
+    console.log('dom mouseover');
+    clearTimeout(timer);
+});
+
+let timer;
+function hidden() {
+    timer = setTimeout(() => {
+        DOM.classList.remove('show');
+    }, 1000);
+}
--- src/popup/index.js
+++ src/popup/index.js
@@ -1,18 +1,22 @@
-import React, { Fragment } from 'react'
+import React, { useEffect } from 'react'
 import './popup.styl'
 // import Home from './pages/home'
 // import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 function Popup() {
+    useEffect(() => {
+        try {
+            let insertScript = document.createElement('script')
+            insertScript.setAttribute('type', 'text/javascript')
+            insertScript.setAttribute('src', '/insert.js')
+            document.body.appendChild(insertScript)
+        } catch (err) {}
+    }, []);
     return (
-        // <HashRouter>
-        //     <Switch>
-        //         <Route key="/login" path="/login" component={Home} />
-        //         <Route path="/home" path="/home" component={Home} />
-        //     </Switch>
-        // </HashRouter>
-        // <Home />
         <></>
+        // <div className='popup'>
+        //     <a className='collection'>收藏</a>
+        // </div>
     )
 }
 
